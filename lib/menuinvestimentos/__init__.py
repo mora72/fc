b27269853d4e2c3957e537902aed25fb54ff9b73
Carlos H Mora Jr <carlosmorajunior@gmail.com>
2020-04-12 22:49:40 -0300

    26o commit FC Mora

--- lib/menuinvestimentos/__init__.py
+++ lib/menuinvestimentos/__init__.py
@@ -1,118 +1,6 @@
 # Funções do Menu INVESTIMENTOS
 from lib.interface import *
-
-
-def newinvest(lista, mestrabalho, anotrabalho):
-    tipoinvest = exibeinvest(lista, mestrabalho, anotrabalho)
-    cabecalho(f'ANO TRABALHO: {anotrabalho} - MES TRABALHO: {mestrabalho}')
-    cabecalho('NOVO INVESTIMENTO')
-    while True:
-        if tipoinvest == '':
-            tipoinvest = input(f'{espacos()}Digite o tipo do investimento: ')
-        else:
-            print(f'{espacos()}Tipo do investimento: {tipoinvest}')
-        nomeinvest = input(f'{espacos()}Digite o nome do investimento: ')
-        diainiinvest = leiaint('Digite o dia do início do investimento: ')
-        mesiniinvest = leiaint('Digite o mês do início do investimento: ')
-        anoiniinvest = leiaint('Digite o ano do início do investimento: ')
-        qtdeini = leiaint('Digite quantidade inicial: ')
-        vlruniini = leiafloat('Digite valor unitário inicial: ')
-        vlrtotini = vlruniini * qtdeini
-        cabecalho(f'CONFIRMA REGISTRO ? ')
-        opcao = input(f'{espacos()}Sim (S) ou Não (N) ? ')
-        if opcao in 'Ss':
-            registro = {'ano': anotrabalho,
-                        'mes': mestrabalho,
-                        'diainiinvest': diainiinvest,
-                        'mesiniinvest': mesiniinvest,
-                        'anoiniinvest': anoiniinvest,
-                        'tipoinvest': tipoinvest,
-                        'nomeinvest': nomeinvest,
-                        'vlruniini': vlruniini,
-                        'qtdeini': qtdeini,
-                        'vlrtotini': vlrtotini,
-                        'qtdefim': 0,
-                        'vlrunifim': 0,
-                        'vlrtotfim': 0,
-                        }
-            lista.append(registro.copy())
-            print(f'{espacos()}REGISTRO INSERIDO')
-        opcao = input(f'{espacos()}Lançar outra transação? Sim (S) ou Não (N) ? ')
-        if opcao in 'Nn':
-            break
-
-
-def exibeinvest(lista, mes, ano, tipoinvest=''):
-    system("cls")
-    margem = 3
-    cabecalho('INVESTIMENTOS DO MES E ANO DE TRABALHO', 150, margem)
-    if tipoinvest == '':
-        opcao = leiaint('Digite 1 para ver todos ou 2 para um tipo específico: ', margem)
-        if opcao == 1:
-            tipoinvest = '*'
-        elif opcao == 2:
-            tipoinvest = input(f'{espacos(margem)}Digite o tipo do investimento: ')
-        else:
-            tipoinvest = ''
-    vlrtotini = 0
-    vlrtotfim = 0
-    for c, x in enumerate(lista):
-        if x["mes"] == mes and x["ano"] == ano and (x["tipoinvest"] == tipoinvest or tipoinvest == '*'):
-            if x["tipoinvest"] != 'Fundo Provisão':
-                vlrtotini += x["vlrtotini"]
-                vlrtotfim += x["vlrtotfim"]
-            print(f'{espacos(margem)}ID: {c:3} - {x["diainiinvest"]:2}/{x["mesiniinvest"]:2}/{x["anoiniinvest"]} '
-                  f'{x["tipoinvest"]:<30} {x["nomeinvest"]:<50} '
-                  f'{x["vlruniini"]:>10,.2f} {x["qtdeini"]:>6,} {x["vlrtotini"]:>10,.2f} '
-                  f'{x["vlrunifim"]:>10,.2f} {x["qtdefim"]:>6,} {x["vlrtotfim"]:>10,.2f} ')
-    print(linha(150, 0))
-    print(f'VALOR TOTAL INICIAL.: {vlrtotini:>10,.2f}')
-    print(f'VALOR TOTAL FINAL...: {vlrtotfim:>10,.2f}')
-    print(linha(150, 0))
-    aguardaenter(margem)
-    return tipoinvest
-
-
-def deletainvest(lista, mes, ano):
-    exibeinvest(lista, mes, ano)
-    cabecalho('Deletar Investimento')
-    while True:
-        idtransacao = leiaint('Digite ID do Investimento que deseja deletar ou -1 para desistir: ')
-        if -1 <= idtransacao <= len(lista) - 1:
-            break
-        else:
-            print(f'{espacos()}ID Inválido !')
-    if idtransacao >= 0:
-        print(f'{espacos()}ID DELETADO !')
-        del lista[idtransacao]
-    else:
-        print(f'{espacos()}Deleção CANCELADA !')
-    aguardaenter()
-
-
-def updateinvest(lista, mes, ano):
-    tipoinvest = ''
-    while True:
-        tipoinvest = exibeinvest(lista, mes, ano, tipoinvest)
-        cabecalho('Atualizar Investimento')
-        while True:
-            idtrans = leiaint('Digite ID do Investimento que deseja atualizar ou -1 para finalizar: ')
-            if -1 <= idtrans <= len(lista) - 1:
-                break
-            else:
-                print(f'{espacos()}ID Inválido !')
-        if idtrans >= 0:
-            lista[idtrans]["vlrunifim"] = leiafloat('Digite novo valor unitário final: ')
-            lista[idtrans]["qtdefim"] = leiaint('Digite nova qtde final: (-1 para manter qtde inicial)')
-            if lista[idtrans]["qtdefim"] == -1:
-                lista[idtrans]["qtdefim"] = lista[idtrans]["qtdeini"]
-            lista[idtrans]["vlrtotfim"] = lista[idtrans]["vlrunifim"] * lista[idtrans]["qtdefim"]
-            print(f'{espacos()}ID ALTERADO !')
-            aguardaenter()
-        else:
-            print(f'{espacos()}Alteração FINALIZADA !')
-            aguardaenter()
-            break
+from lib.arquivos import *
 
 
 def exibeinvestnew(listainvest, mes, ano, tipoinvest=''):
@@ -182,6 +70,8 @@ def invest(listainvest, mes, ano):
                             'vlrtotfim': 0,
                             }
                 listainvest.append(registro.copy())
+                arqlistainvest = Arquivolista('/Users/carlo/PycharmProjects/fc/baseinvest.pck1', 'Investimentos')
+                arqlistainvest.gravar(listainvest)
         elif opcao == 2:
             bordasup(3)
             cabecalho('Deletar Investimento', 42, margem)
@@ -192,7 +82,12 @@ def invest(listainvest, mes, ano):
                 else:
                     print(f'{espacos(margem)}ID Inválido !')
             if idtransacao >= 0:
-                del listainvest[idtransacao]
+                cabecalho(f'CONFIRMA ALTERAÇÃO ? ', 42, margem)
+                opcao = input(f'{espacos(margem)}Sim (S) ou Não (N) ? ')
+                if opcao in 'Ss':
+                    del listainvest[idtransacao]
+                    arqlistainvest = Arquivolista('/Users/carlo/PycharmProjects/fc/baseinvest.pck1', 'Investimentos')
+                    arqlistainvest.gravar(listainvest)
         elif opcao == 3:
             bordasup(3)
             cabecalho(' MUDAR TIPO DE INVESTIMENTO', 42, margem)
@@ -208,7 +103,7 @@ def invest(listainvest, mes, ano):
             if idtrans >= 0:
                 new_vl = leiafloat('Valor unitário final: ', margem)
                 new_qt = leiaint('Qtde final: (-1 para = inicial)', margem)
-                cabecalho(f'CONFIRMA REGISTRO ? ', 42, margem)
+                cabecalho(f'CONFIRMA ALTERAÇÃO ? ', 42, margem)
                 opcao = input(f'{espacos(margem)}Sim (S) ou Não (N) ? ')
                 if opcao in 'Ss':
                     listainvest[idtrans]["vlrunifim"] = new_vl
@@ -216,6 +111,9 @@ def invest(listainvest, mes, ano):
                         listainvest[idtrans]["qtdefim"] = listainvest[idtrans]["qtdeini"]
                     else:
                         listainvest[idtrans]["qtdefim"] = new_qt
-                    listainvest[idtrans]["vlrtotfim"] = listainvest[idtrans]["vlrunifim"] * listainvest[idtrans]["qtdefim"]
+                    listainvest[idtrans]["vlrtotfim"] = listainvest[idtrans]["vlrunifim"] * \
+                                                        listainvest[idtrans]["qtdefim"]
+                    arqlistainvest = Arquivolista('/Users/carlo/PycharmProjects/fc/baseinvest.pck1', 'Investimentos')
+                    arqlistainvest.gravar(listainvest)
         elif opcao == 9:
             break
